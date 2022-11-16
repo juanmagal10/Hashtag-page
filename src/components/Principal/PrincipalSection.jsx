ace6a4d91ac7b792f92df997c5bc750251be9001
juanmagal10 <juanmagalvan.24@gmail.com>
2022-11-16 00:11:43 -0300

    images placed

--- src/components/Principal/PrincipalSection.jsx
+++ src/components/Principal/PrincipalSection.jsx
@@ -20,14 +20,21 @@ const PrincipalSection = () => {
   return (
     <section className='principal-section-container'>
       <div className="left-img-container">
-        <img src={img} alt='profile picture' loading='lazy' className='left-profile-pic profile-pic' />
-        <h2 className='profile-name-left'>Clarisa Oviedo</h2>
+        <div className="img-text-container">
+          <img src={img} alt='profile picture' loading='lazy' className='left-profile-pic profile-pic' />
+          <h2 className='profile-name-left'>Clarisa Oviedo</h2>
+        </div>
       </div>
 
-      <div className="right-img-container">
-        <img src={img} alt='profile picture' loading='lazy' className='right-profile-pic' />
-        <h2 className='profile-name-right'>Clarisa Oviedo</h2>
-      </div>
+        <div className="right-img-container">
+          <div className="img-text-container">
+            <img src={img} alt='profile picture' loading='lazy' className='right-profile-pic' />
+            <h2 className='profile-name-right'>Clarisa Oviedo</h2>
+          </div>
+        </div>
+      
+        
+     
     </section>
   )
 }
