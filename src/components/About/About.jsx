2099b3f7eeffaf1dfaf6b20a5f6ac361d5658239
juanmagal10 <juanmagalvan.24@gmail.com>
2022-11-28 16:44:51 -0300

    about with real data

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,14 +10,17 @@ const About = () => {
         </div>
         <div className="about-text-container">
           <p className='about-text'>¡HOLA! Somos Clari y Ro 👭🏻, juntas conformamos el Team Hashtag Agencia de Marketing Digital 🖥.
-          Decidimos unir nuestras profesiones, fotógrafa 📸  y Diseñadora Gráfica 👩🏻‍💻 para hacer crecer sus negocios en el medio digital y llevarlos a otro nivel 📈</p>
-        <h5>Como hacemos crecer tu marca?</h5>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+          Decidimos unir nuestras profesiones, fotógrafa 📸  y Diseñadora Gráfica 👩🏻‍💻 para hacer crecer sus negocios en el medio digital y llevarlos a otro nivel 📈.</p>
+          <h5>¿Como hacemos crecer tu marca?</h5>
+          <ul className='about-list'>
+            <li className='about-list-item'>Gestion de redes</li>
+            <li className='about-list-item'>Consultoria</li>
+            <li className='about-list-item'>Publicidad en medios sociales</li>
+            <li className='about-list-item'>Fotoproducto/Producción de fotos y videos</li>
+            <li className='about-list-item'>Diseño/Branding</li>
+            <li className='about-list-item'>Creación de reels, gift, filtros y carrusel</li>
+            <li className='about-list-item'>Diseño de historias y post</li>
+          </ul>
         </div>
     </article>
   )
