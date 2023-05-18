1ebdf1e97d497b1db77da0b3116579307c724936
juanmagal10 <juanmagalvan.24@gmail.com>
2023-05-18 11:46:45 -0300

    services itemd organiced

--- src/components/servicios/Servicios.jsx
+++ src/components/servicios/Servicios.jsx
@@ -12,40 +12,82 @@ const Servicios = () => {
               <h2 className='servicios-title'>Servicios que ofrecemos:</h2>
           </div>
           <div className="servicios-grid-container">
+              
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[0].nombre}</h3>
-                  <img src={servicesData[0].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[0].descripcion}</p>
+                   <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[0].nombre}</h3>
+                  </div>
+                  <div className="servicio-imagen-container">
+                    <img src={servicesData[0].img} alt="" className='servicio-imagen' />
+                  </div>
+                  <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[0].descripcion}</p>
+                  </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
+
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[1].nombre}</h3>
-                  <img src={servicesData[1].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[1].descripcion}</p>
+                   <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[1].nombre}</h3>
+                  </div>
+                     <div className="servicio-imagen-container">
+                    <img src={servicesData[1].img} alt="" className='servicio-imagen' />
+                  </div>
+                  <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[1].descripcion}</p>
+                  </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
+
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[2].nombre}</h3>
-                  <img src={servicesData[2].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[2].descripcion}</p>
+                <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[2].nombre}</h3>
+                </div>
+                <div className="servicio-imagen-container">
+                    <img src={servicesData[2].img} alt="" className='servicio-imagen' />
+                </div>
+                <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[2].descripcion}</p>
+                </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
+
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[3].nombre}</h3>
-                  <img src={servicesData[3].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[3].descripcion}</p>
+                <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[3].nombre}</h3>
+                </div>
+                   <div className="servicio-imagen-container">
+                    <img src={servicesData[2].img} alt="" className='servicio-imagen' />
+                </div>
+                <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[2].descripcion}</p>
+                </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
+
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[4].nombre}</h3>
-                  <img src={servicesData[4].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[4].descripcion}</p>
+                   <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[4].nombre}</h3>
+                  </div>
+                    <div className="servicio-imagen-container">
+                    <img src={servicesData[4].img} alt="" className='servicio-imagen' />
+                  </div>
+                  <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[4].descripcion}</p>
+                  </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
+
               <div className="grid-service-item">
-                  <h3 className='single-servicio-title'>{servicesData[5].nombre}</h3>
-                  <img src={servicesData[5].img} alt="" className='servicio-imagen' />
-                  <p className='single-servicio-text'>{servicesData[5].descripcion}</p>
+                   <div className="single-servicio-title-container">
+                    <h3 className='single-servicio-title'>{servicesData[5].nombre}</h3>
+                  </div>
+                    <div className="servicio-imagen-container">
+                    <img src={servicesData[5].img} alt="" className='servicio-imagen' />
+                  </div>
+                  <div className="single-servicio-text-container">
+                    <p className='single-servicio-text'>{servicesData[5].descripcion}</p>
+                  </div>
                   {/* <button className='servicios-link-btn'><Link to={`/${servicesData[0].id}`}>Mas info</Link></button> */}
               </div>
           </div>
